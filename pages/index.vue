c8321f732f6d088d02dab950d28b79b805bf2b3c
Josue Hernandez Ramirez <ingscjoshua@gamil.com>
2024-07-24 22:30:27 -0600

    add base files

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <Button icon="pi pi-home" label="Save" />
-
+    <Button icon="pi pi-home" label="Save" />
 </template>
 <script lang="ts">
 import Button from 'primevue/button';
-export  default {
-
-}
+export default {};
 </script>
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
